7574d50a525bdfb1145e8533b916e41e12a37b38
LynithDev <61880709+LynithDev@users.noreply.github.com>
2023-12-03 13:54:28 +0100

    Fixes + doesn't enable when link clicked

--- src/injected/index.ts
+++ src/injected/index.ts
@@ -1,13 +1,18 @@
 let activated: boolean = false;
-let element: HTMLDivElement | null = null;
+let element: HTMLElement | null = null;
 let focusedElement: Element | null = null;
 let startingX: number = 0;
 let startingY: number = 0;
 let currentX: number = 0;
 let currentY: number = 0;
 
-function toggle(e: MouseEvent) {
-    activated = !activated;
+function toggle(e: MouseEvent, force: boolean | null = null) {
+    if (force != null) {
+        activated = force;
+    } else {
+        activated = !activated;
+    }
+    
     if (activated) {
         focusedElement = searchForScrollableElement(e.target as HTMLElement);
         onEnable();
@@ -39,7 +44,11 @@ function scrollLoop(delta: number) {
     const scrollY = dy * speed * -1;
 
     if (!focusedElement) focusedElement = document.documentElement;
-    focusedElement.scrollBy(scrollX, scrollY);
+    focusedElement.scrollTo({
+        top: focusedElement.scrollTop + scrollY,
+        left: focusedElement.scrollLeft + scrollX,
+        behavior: "instant"
+    });
 
     requestAnimationFrame(scrollLoop);
 }
@@ -47,21 +56,42 @@ function scrollLoop(delta: number) {
 function onMouseMove(ev: MouseEvent) {
     currentX = ev.clientX;
     currentY = ev.clientY;
-    const direction = getDirection();
-    document.body.style.cursor = getCursorIcon(direction);
+    // const direction = getDirection();
+    // document.body.style.cursor = getCursorIcon(direction);
 }
 
 function onEnable() {
-    element = document.createElement("div");
-    element.innerHTML = createElement({ x: startingX, y: startingY });
+    element = createElement({ x: startingX, y: startingY });
     document.body.appendChild(element);
 
     window.addEventListener("mousemove", onMouseMove);
     requestAnimationFrame(scrollLoop);
 }
 
-function createElement({ x = 0, y = 0 }): string {
-    return `<div style="position:fixed;z-index:9999999;top:${y-15}px;left:${x-15}px;opacity:0.8"><img src=${chrome.runtime.getURL("images/middleclick.png")} style="filter:none;border:2px solid #3f3f3f;border-radius:99999px" /></div>`;
+function createElement({ x = 0, y = 0 }): HTMLElement {
+    const element = document.createElement("iframe");
+
+    element.style.position = "fixed";
+    element.style.zIndex = "999999999999";
+    element.style.top = `${y - 15}px`;
+    element.style.left = `${x - 16}px`;
+    element.style.opacity = "0.8";
+    element.style.pointerEvents = "none";
+    element.style.filter = "none";
+    element.style.border = "2px solid #3f3f3f";
+    element.style.borderRadius = "99999px";
+    element.width = "33";
+    element.height = "33";
+    element.style.width = "33px";
+    element.style.height = "33px";
+    element.style.boxSizing = "border-box";
+    element.style.overflow = "hidden";
+    element.frameBorder = "no";
+    element.scrolling = "no";
+
+    element.src = chrome.runtime.getURL("images/middleclick.png");
+
+    return element;
 }
 
 function onDisable() {
@@ -81,6 +111,8 @@ function onDisable() {
 function onWheelClick(ev: MouseEvent) {
     ev.preventDefault();
     ev.stopPropagation();
+    ev.stopImmediatePropagation();
+    ev.returnValue = false;
 
     currentX = ev.clientX;
     currentY = ev.clientY;
@@ -94,24 +126,30 @@ let clickTime: number | null = null;
 window.addEventListener("mousedown", (ev) => {
     if (ev.button !== 1) {
         if (activated) {
-            toggle(ev);
+            toggle(ev, false);
         }
 
+        return;
+    }
+    
+    if (isPartOfAnchor(ev.target as HTMLElement)) {
+        clickTime = null;
         return;
     }
 
     clickTime = Date.now();
     onWheelClick(ev);
-})
+});
 
 window.addEventListener("mouseup", (ev) => {
     if (ev.button !== 1) return;
 
     const time = Date.now();
-    if (clickTime && time - clickTime > 350) {
+    if (clickTime && time - clickTime > 250) {
         onWheelClick(ev);
     }
 });
+
 
 // Utils
 const directions = ["E", "SE", "S", "SW", "W", "NW", "N", "NE"] as const;
@@ -119,6 +157,18 @@ type Direction = typeof directions[number];
 
 function getCursorIcon(direction: Direction): string {
     return direction.toLowerCase() + "-resize";
+}
+
+function isPartOfAnchor(element: HTMLElement): boolean {
+    if (element.nodeName === "A") {
+        return true;
+    }
+
+    if (element.nodeName === "BODY" || element.parentElement == null) {
+        return false;
+    }
+
+    return isPartOfAnchor(element.parentElement);
 }
 
 function getDirection(): Direction  {
